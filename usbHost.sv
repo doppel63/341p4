9ec2467a2bbf288197cde678148ada2ad760375d
linky <xianglin@andrew.cmu.edu>
2013-11-12 22:11:27 -0500

    separated tb stuff

--- usbHost.sv
+++ usbHost.sv
@@ -180,72 +180,6 @@ module bitStreamEncoder(
 
 endmodule
 
-// test bit stream encoder
-module bitStreamEncoder_tb;
-  logic        clk, rst;
-  logic        pkt_avail;
-  logic [7:0]  pid_in;
-  logic [6:0]  addr_in;
-  logic [3:0]  endp_in;
-  logic [63:0] data_in;
-  logic        stall;
-  logic        bit_out, start, last;
-
-  logic [95:0]  result;   // largest is SYNC + PID + DATA0 + CRC16
-
-  bitStreamEncoder dut(.*);
-
-  initial begin
-    clk = 1;
-    forever #1 clk = ~clk;
-  end
-
-  // used for keeping track of the bit stream
-  // note that result is always 1 clock cycle late!
-  always_ff @(posedge clk, posedge rst) begin
-    if (rst)
-      result <= 0;
-    else if (~stall) begin
-      result <= result << 1;
-      result[0] <= bit_out;
-    end
-  end
-
-  initial begin
-    rst <= 1; @(posedge clk);
-    rst <= 0; @(posedge clk);
-    // test sending OUT to endpoint 4, data = CAFEBABEDEADBEEF
-    $monitor($time,, "stall = %b, start = %b, last = %b, result = %h",
-                      stall, start, last, result);
-    stall <= 0;
-    pid_in <= 8'b1110_0001; addr_in <= 5; endp_in <= 4;
-    pkt_avail <= 1;
-    $display("SENDING OUT to endpoint 4");
-    @(posedge clk);
-    pkt_avail <= 0;
-    repeat (33) @(posedge clk);
-    // test stall
-    rst <= 1; @(posedge clk);
-    rst <= 0; @(posedge clk);
-    pkt_avail <= 1;
-    $display("SENDING OUT to endpoint 4 with stall");
-    @(posedge clk);
-    pkt_avail <= 0;
-    repeat (24) @(posedge clk);
-    stall <= 1; repeat (5) @(posedge clk);
-    stall <= 0; repeat (9) @(posedge clk);
-    // test sending DATA
-    $display("SENDING DATA = CAFEBABEDEADBEEF");
-    pid_in <= 8'b1100_0011; data_in <= 64'hCAFEBABEDEADBEEF;
-    pkt_avail <= 1;
-    @(posedge clk);
-    pkt_avail <= 0;
-    repeat (97) @(posedge clk);
-    $finish;
-  end
-
-endmodule
-
 // CRC stuff
 // CRC5 calculator: rem 01100
 module crc5Calc(
@@ -625,44 +559,6 @@ module bitStuffer(
         default:  state <= state;
       endcase
     end
-  end
-
-endmodule
-
-module bitStuffer_tb;
-  logic clk, rst;
-  logic start;
-  logic last;
-  logic bit_in;
-  logic bit_out;
-  logic stall;
-
-  bitStuffer dut(.*);
-
-  initial begin
-    clk = 0;
-    forever #5 clk = ~clk;
-  end
-
-  initial begin
-    $monitor($time,, "bit_in = %b, bit_out = %b, stall = %b",
-                      bit_in, bit_out, stall);
-    rst <= 1; @(posedge clk);
-    rst <= 0;
-    start <= 0; last <= 0;
-    // test some sequence: 011001111111111
-    // should get:         0110011111101111
-    bit_in <= 0;  start <= 1; @(posedge clk);
-    bit_in <= 1;  start <= 0; @(posedge clk);
-    bit_in <= 1; @(posedge clk);
-    bit_in <= 0; @(posedge clk);
-    bit_in <= 0; @(posedge clk);
-    repeat (6) begin bit_in <= 1; @(posedge clk); end
-    @(posedge clk);
-    repeat (3) begin bit_in <= 1; @(posedge clk); end
-    bit_in <= 1;  last <= 1; @(posedge clk);
-    last <= 0; @(posedge clk);
-    $finish;
   end
 
 endmodule
